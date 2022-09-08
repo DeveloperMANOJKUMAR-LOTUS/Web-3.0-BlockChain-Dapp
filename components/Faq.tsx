0fcf5761b577e6fffb59596afaf2ea0c7284d209
Manojkumar <developermanoj2003@gmail.com>
2022-09-08 10:11:05 +0530

    added all

---
+++ components/Faq.tsx
@@ -0,0 +1,16 @@
+export default function Faq() {
+  return (
+    <>
+      <h2 className="text-4xl mb-8">FAQ</h2>
+
+      <div className="space-y-4">
+        <div>
+          <h3 className="text-2xl mb-2">How much is the minting price?</h3>
+          <p>
+            The minting price of the NFT can be calculated and it depends upon the trend and the value of the NFT . 
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
